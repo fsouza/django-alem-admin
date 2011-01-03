069ee5b267ebb2717d6fad0e4a0711638f31714d
Francisco Souza <francisco@franciscosouza.net>
2011-01-02 22:23:29 -0200

    Adição do model Product

--- vitriny/core/models.py
+++ vitriny/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.FloatField()
+    creation_date = models.DateField(auto_now_add=True)
+    last_modification_date = models.DateField(auto_now=True)
+    hidden = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='images/products',
+                              max_length=200)
