0879fec37918984c3c036dc046b874a9052f705a
Francisco Souza <francisco@franciscosouza.net>
2011-01-02 22:57:08 -0200

    Added a non-generic view for new products

--- vitriny/core/views.py
+++ vitriny/core/views.py
@@ -1,1 +1,24 @@
 # Create your views here.
+from models import Product
+from django import forms
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+
+def new_product(request):
+    """Renders a product form on GET requests and
+    saves a product on POST requests"""
+    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Product saved.')
+    return render_to_response('product_form.html', {
+            'form' : form
+        }, context_instance=RequestContext(request)
+    )
--- vitriny/urls.py
+++ vitriny/urls.py
@@ -13,4 +13,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
+    (r'^new_product', 'core.views.new_product'),
 )
