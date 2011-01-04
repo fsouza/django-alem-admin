a92dae5c7361a58c8c8aa1f33aaf38fa123c6904
Francisco Souza <francisco@franciscosouza.net>
2011-01-04 18:22:01 -0200

    Adição de classe de formulário de contato

--- vitriny/core/views.py
+++ vitriny/core/views.py
@@ -5,10 +5,17 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
+from django.contrib.localflavor.br.forms import BRCPFField
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
+
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=150)
+    email = forms.EmailField(max_length=75)
+    cpf = BRCPFField()
+    message = forms.TextField()
 
 def new_product(request):
     """Renders a product form on GET requests and
